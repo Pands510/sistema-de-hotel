a5ebca8810c358ac03d7e3b73a815f51c2ba3bab
Pands510 <capaivaramdl@gmail.com>
2026-07-07 19:51:37 -0300

    classe Pessoa implementada

--- Pessoa.java
+++ Pessoa.java
@@ -1,3 +1,40 @@
 public abstract class Pessoa {
+    private String nome;
+    private final String cpf;
+    
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
 
+        if(!validarCpf(cpf)){
+            throw new IllegalArgumentException("formato invalido");
+        }
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        if(nome == null || nome.isBlank()){
+            throw new IllegalArgumentException("formato invalido");
+        }
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+
+    public boolean validarCpf(String cpf){
+        if(cpf == null || cpf.isBlank()){
+            return false;
+        }
+        return cpf.matches("\\d{11}");
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa" +
+        "\nnnome: " + nome +
+        "\ncpf: " + cpf;
+    }
 }
