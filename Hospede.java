4ca6e79bc5aa81ff21d47353dc2bb5ee3719150c
Pands510 <capaivaramdl@gmail.com>
2026-07-07 19:55:59 -0300

    classe Hospede implementada

--- Hospede.java
+++ Hospede.java
@@ -1,3 +1,16 @@
 public class Hospede extends Pessoa implements Pesquisavel{
-    
+
+    public Hospede(String nome, String cpf) {
+        super(nome, cpf);
+    }
+
+    @Override
+    public boolean pesquisar(String texto) {
+        if(texto == null || texto.isBlank()){
+            throw new IllegalArgumentException("formato invalido");
+        }
+        if(getNome().toLowerCase().contains(texto) || getCpf().contains(texto)){
+            return true;
+        } return false;
+    }
 }
