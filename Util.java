c6a611798b0b03135fb0b3088193524e8e4cb071
Pands510 <capaivaramdl@gmail.com>
2026-07-07 20:14:49 -0300

    classe Utils implementada

--- Util.java
+++ Util.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class Util {
-    
+    private static final Scanner sc = new Scanner(System.in);
+
+    private static void linha(){
+        System.out.println("==================");
+    }
+
+    public static String lerTexto(){
+        return sc.nextLine();
+    }
+
+    public static int lerInteiro(){
+        int inteiro = sc.nextInt();
+        sc.nextLine();
+        return inteiro;
+    }
+
+    public static double lerDouble(){
+        double val = sc.nextDouble();
+        sc.nextLine();
+        return val;
+    }
+
 }
